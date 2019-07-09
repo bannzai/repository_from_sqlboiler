15d6d20ac2ccd18cd863df40b2876b3f25c87c30
bannzai <kingkong999yhirose@gmail.com>
2019-07-09 16:14:02 +0900

    :sparkels: Add file writer

---
+++ pkg/writer/file.go
@@ -0,0 +1,22 @@
+package writer
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type File struct {
+	FilePath string
+}
+
+type FileWriter interface {
+	Write(content string)
+}
+
+func (f File) WriteFile(content string) {
+	if err := ioutil.WriteFile(f.FilePath, []byte(content), 0644); err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(os.Stdout, "Generated %s. \n", f.FilePath)
+}

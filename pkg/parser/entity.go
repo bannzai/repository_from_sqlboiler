ed7f15021b27d3b2b1db21b48c3eaec7648e0fdb
bannzai <kingkong999yhirose@gmail.com>
2019-07-10 11:29:28 +0900

    :sparkles: Fix parser

--- pkg/parser/entity.go
+++ pkg/parser/entity.go
@@ -1,18 +1,20 @@
 package parser
 
 import (
+	"path/filepath"
+
 	"github.com/bannzai/repository_from_sqlboiler/pkg/model"
+	"github.com/bannzai/repository_from_sqlboiler/pkg/strutil"
 )
 
 type Entity struct {
-	EntityName string
-	FilePath   string
+	FilePath string
 	FileReader
 }
 
 func (p Entity) Parse() model.Entity {
 	return model.Entity{
-		Name:        p.EntityName,
+		Name:        p.extractEntityName(),
 		Fields:      p.mapFields(),
 		PrimaryKeys: p.mapPrimaryKeys(),
 	}
@@ -20,7 +22,7 @@ func (p Entity) Parse() model.Entity {
 
 func (p Entity) mapPrimaryKeys() []model.PrimaryKey {
 	primaryKeys := []model.PrimaryKey{}
-	for _, primaryKeyName := range parseASTFieldAndType(parseASTFile(p.FilePath), p.extractStructName()) {
+	for _, primaryKeyName := range parseASTFieldAndType(parseASTFile(p.FilePath), p.extractEntityName()) {
 		primaryKeys = append(
 			primaryKeys,
 			model.PrimaryKey{
@@ -32,11 +34,19 @@ func (p Entity) mapPrimaryKeys() []model.PrimaryKey {
 
 func (p Entity) mapFields() []model.Field {
 	fields := []model.Field{}
-	for fieldName, typeName := range parseASTFieldAndType(parseASTFile(p.FilePath), p.extractStructName()) {
+	for fieldName, typeName := range parseASTFieldAndType(parseASTFile(p.FilePath), p.extractEntityName()) {
 		fields = append(fields, model.Field{
 			Name:     fieldName,
 			TypeName: typeName,
 		})
 	}
 	return fields
+}
+
+func (e Entity) extractEntityName() string {
+	filename := filepath.Base(e.FilePath)
+	extension := filepath.Ext(e.FilePath)
+	snakeCaseEntityName := filename[0 : len(filename)-len(extension)]
+	entityName := strutil.UpperCamelCase(snakeCaseEntityName)
+	return entityName
 }

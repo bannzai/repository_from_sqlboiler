160c865bd74fe7af758a77f64877b51d51d2681a
bannzai <kingkong999yhirose@gmail.com>
2019-07-12 13:23:27 +0900

    :recycle: Separate file for plural test

--- pkg/strutil/name_test.go
+++ pkg/strutil/name_test.go
@@ -240,36 +240,3 @@ func TestEscapedReservedWord(t *testing.T) {
 		})
 	}
 }
-
-func TestPlural(t *testing.T) {
-	type args struct {
-		str string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "user to users",
-			args: args{
-				str: "user",
-			},
-			want: "users",
-		},
-		{
-			name: "community to communities",
-			args: args{
-				str: "community",
-			},
-			want: "communities",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Plural(tt.args.str); got != tt.want {
-				t.Errorf("Plural() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}

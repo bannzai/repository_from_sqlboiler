14f27c6d92c13857d0269bb615c6a5ad8f40e3c2
bannzai <kingkong999yhirose@gmail.com>
2019-07-12 13:22:18 +0900

    :recycle: Separate file for plural

--- pkg/strutil/name.go
+++ pkg/strutil/name.go
@@ -73,28 +73,3 @@ func SpecializeUpperCamelCase(str string) string {
 	}
 	return UpperCamelCase(str)
 }
-
-func Plural(str string) string {
-	if len(str) < 2 {
-		return str
-	}
-	if str[len(str)-1:] == "s" ||
-		str[len(str)-2:] == "sh" ||
-		str[len(str)-2:] == "ch" ||
-		str[len(str)-1:] == "o" ||
-		str[len(str)-1:] == "x" {
-		return str + "es"
-	}
-	if str[len(str)-1:] == "f" {
-		return str[0:len(str)-1] + "ves"
-	}
-	if str[len(str)-2:] == "fe" {
-		return str[0:len(str)-2] + "ves"
-	}
-	isExists, _ := regexp.MatchString(".+[^aiueo]y$", str)
-	if isExists {
-		return str[0:len(str)-1] + "ies"
-	}
-
-	return str + "s"
-}
